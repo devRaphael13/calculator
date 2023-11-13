5b92bb6653de5cf8ba6b46bbe4668f813c4f3b61
Raphael Ezeigwe <iraphael1308@gmail.com>
2023-11-13 13:43:04 +0100

    Add comments

--- scripts.js
+++ scripts.js
@@ -5,17 +5,28 @@ const answer = document.getElementById("answer");
 const signs = ["=", "+", "-", "x", "/", "%", "."];
 const config = { characterData: true, subtree: true, childList: true };
 
+document.addEventListener("DOMContentLoaded", () => operate());
+
+// Listen to changes in the question element.
 const observer = new MutationObserver((mutations) => {
+
+    // Get display as a string
     let display = mutations[0].addedNodes[0]?.textContent.trimEnd();
     let ram = [];
 
     ram.push(...clean(display));
+
+    // Check for abnormal sign usage
     ram = validateSign(ram);
+
+    // Allow 2 operand calculation for percent
     if (ram.length > 1 && ram.slice(-1) == "%") {
         let ans = calculate(...ram);
         updateQuestion(ans);
         showAnswer(ans);
     }
+
+    // Normal 3 operand calculation
     if (ram.length > 3) {
         let ans = calculate(...ram);
         let lastSign = ram[3];
@@ -27,16 +38,21 @@ const observer = new MutationObserver((mutations) => {
 });
 
 observer.observe(question, config);
-// observer.disconnect()
 
 function validateSign(ram) {
     const preSigns = ["+", "-"];
+
+    // Refuse random signs in front
     if (signs.includes(ram[0]) && !preSigns.includes(ram[0]))
-        updateQuestion("");
+    updateQuestion("");
+
+    // Allow operations with negative numbers
     else if (preSigns.includes(ram[0]) && ram[1]) {
         let [sign, num] = ram.splice(0, 2);
         ram.unshift(Number.parseFloat(`${sign}${num}`));
     }
+
+    // Refuse 2 signs together
     ram.filter((element, i, array) => {
         if (signs.includes(array[i]) && signs.includes(array[i - 1])) {
             array.splice(i - 1, 1);
@@ -52,10 +68,14 @@ function updateQuestion(num, lastSign = null) {
         ? ""
         : num;
 }
+
 function showAnswer(num) {
     answer.textContent = num;
 }
+
 function clean(display) {
+
+    // Leave signs & convert the rest to numbers
     if (display) {
         display = display
             .split(" ")
@@ -69,26 +89,6 @@ function clean(display) {
     return display;
 }
 
-document.addEventListener("DOMContentLoaded", () => operate());
-
-function operate() {
-    readyKeypad();
-}
-
-function readyKeypad() {
-    for (let key of keypad) {
-        key.addEventListener("click", (e) => {
-            // TODO sign,
-            if (e.target.textContent === "AC") clear();
-            else if (e.target.textContent === "Del") del();
-            else {
-                let space = e.target.dataset.number === "false" ? " " : "";
-                question.textContent += space + e.target.textContent + space;
-            }
-        });
-    }
-}
-
 function calculate(a, sign, b = null) {
     const max = 999999999999;
     let ans;
@@ -99,6 +99,27 @@ function calculate(a, sign, b = null) {
     else if (sign === "%") ans = a / 100;
     else return (ans = 0);
     return ans < max ? Number.parseFloat(ans.toFixed(10)) : "E";
+}
+
+function operate() {
+    readyKeypad();
+}
+
+function readyKeypad() {
+    for (let key of keypad) {
+        key.addEventListener("click", (e) => {
+
+            // Handle some modifying characters
+            if (e.target.textContent === "AC") clear();
+            else if (e.target.textContent === "Del") del();
+            else {
+
+                // format text on screen
+                let space = e.target.dataset.number === "false" ? " " : "";
+                question.textContent += space + e.target.textContent + space;
+            }
+        });
+    }
 }
 
 function clear() {
